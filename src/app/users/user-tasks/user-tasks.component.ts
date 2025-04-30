be4d7479d9dc0169102251cc1b49f62853d534e6
Abdul_Malik <malik.info28@gmail.com>
2025-04-30 22:08:15 +0530

    added resolver to extracct userName through userId

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
 
 import { UsersService } from '../users.service';
 
@@ -10,32 +10,39 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit {
-  // userId = input.required<string>();
-  userName = '';
+export class UserTasksComponent {
+  // // userId = input.required<string>();
+  // userName = '';
+  userName = input.required<string>();
 
   message = input.required<string>();
 
-  private usersService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
+  // private usersService = inject(UsersService);
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
 
-  // userName = computed(
-  //   () => this.usersService.users.find(u => u.id === this.userId())?.name
-  // );
+  // // userName = computed(
+  // //   () => this.usersService.users.find(u => u.id === this.userId())?.name
+  // // );
 
-  ngOnInit() {
-    console.log(`Input Data: ${this.message()}`);
-    // console.log(this.activatedRoute);
-    // console.log(this.activatedRoute.snapshot);
-    console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
+  //   // ngOnInit() {
+  //   //   console.log(`Input Data: ${this.message()}`);
+  //   //   // console.log(this.activatedRoute);
+  //   //   // console.log(this.activatedRoute.snapshot);
+  //   //   console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
 
-    const subscribtion = this.activatedRoute.paramMap.subscribe({
-      next: (paramMap) => {
-        this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || 'hi';
-      }
-    });
+  //   //   const subscribtion = this.activatedRoute.paramMap.subscribe({
+  //   //     next: (paramMap) => {
+  //   //       this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || '';
+  //   //     }
+  //   //   });
 
-    this.destroyRef.onDestroy(() => subscribtion.unsubscribe());
-  }
+  //   //   this.destroyRef.onDestroy(() => subscribtion.unsubscribe());
+  //   // }
 }
+
+export const resolveUserName: ResolveFn<string> = (activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot) => {
+  const usersService = inject(UsersService);
+  const userName = usersService.users.find(u => u.id === activatedRoute.paramMap.get('userId'))?.name || 'hi';
+  return userName;
+};
